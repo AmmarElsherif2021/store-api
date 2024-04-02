70e81b02cc913d19170866d2ea5519149d7f626e
Ammar <ammar.alshareef2021@outlook.com>
2024-04-02 02:39:32 +0200

    t

---
+++ models/product.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Product name must be provided!']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Provide price!']
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    company: {
+        type: String,
+        enums: {
+            values: ['ikea', 'liddy', 'caressa', 'marcos']
+        },
+        message: '{VALUE} is not allowed'
+    }
+})
+module.exports = mongoose.model('Product', schema)
